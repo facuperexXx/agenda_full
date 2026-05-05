90997853bb07409bb7ac10ce6e654b32692f23f0
facundo <00facuperez@gmail.com>
2026-05-05 18:13:38 -0300

    se elimina carpeta frontend

--- front-agenda/src/components/ContactosPage.jsx
+++
@@ -1,8 +0,0 @@
-
-function ContactosPage() {
-    return (
-        <h3>Seccion Contactos</h3>
-    )
-}
-
-export default ContactosPage
--- front-agenda/src/components/LocalidadPage.jsx
+++
@@ -1,8 +0,0 @@
-
-function LocalidadPage() {
-    return (
-        <h3>Seccion Localidad</h3>
-    )
-}
-
-export default LocalidadPage
